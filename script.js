402db22c999376539cdb747552dbcca17b5d5a57
Aaron Ray <aaray619@gmail.com>
2020-10-07 09:48:53 -0500

    commiting to merge new files

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-// helloworld
+// helloworld      var queryUrl = "https://www.googleapis.com/civicinfo/v2/elections?key=AIzaSyDDcoCWMnPNLsXimjEmRL85TOfhM9yPsA8"
+src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js";        
+
