fa26b4545223733280b3f6e9f563c54cc0965c51
Aaron Ray <aaray619@gmail.com>
2020-10-07 13:25:03 -0500

    pulled from API and displayed on page. More info still in progress.

--- scripts/pollinglocations.js
+++ scripts/pollinglocations.js
@@ -1,23 +1,44 @@
-src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js";      
 
-var userAddress = $("#findtext").val().trim();
-$.ajax({
-  url: "https://civicinfo.googleapis.com/civicinfo/v2/voterinfo?address=" + userAddress + "&key=AIzaSyDDcoCWMnPNLsXimjEmRL85TOfhM9yPsA8",
-  method: "GET",
-}).then(function(response) {
-  console.log(response)
-  for( i=0; i <11; i++){
-    var newDiv = $("<div>");
-    var pollName = $("<div>").text(response.earlyVoteSites[i].address.locationName);
-    var pollAddressCity = response.earlyVoteSites[i].address.city
-    var pollAddressLine1 = response.earlyVoteSites[i].address.line1
-    var pollAddressState = response.earlyVoteSites[i].address.state
-    var pollAddress =  pollAddressLine1 + " " + pollAddressCity + ", " + pollAddressState;
-    var address = $("<div>").text(pollAddress);
-    var directions = $("<button>").text("Find Directions");
+// create click event for the submit address button
+$("#submitAddress").click(function(e){
+    e.preventDefault();
+    //get user address from input field.
+    var userAddress = $("#findtext").val().trim();
+    // acces the Google Civic jquery
+    $.ajax({
+        url: "https://civicinfo.googleapis.com/civicinfo/v2/voterinfo?address=" + userAddress + "&key=AIzaSyDDcoCWMnPNLsXimjEmRL85TOfhM9yPsA8",
+        method: "GET",
+    }).then(function(response) {
+        console.log(response)
+        // create a for loop that loops 10 times
+        for( i=0; i <11; i++){
+            //create a new div to hold polling locations info
+            var newDiv = $("<div>");
+            newDiv.attr("class", "polling-locations");
+            //call info from APi and create new div for each info with own class.
+            var pollName = $("<div>").text(response.earlyVoteSites[i].address.locationName);
+            pollName.attr("class", "location-name")
+            
+            var pollAddressCity = response.earlyVoteSites[i].address.city
+            var pollAddressLine1 = response.earlyVoteSites[i].address.line1
+            var pollAddressState = response.earlyVoteSites[i].address.state
+            var pollAddress =  pollAddressLine1 + " " + pollAddressCity + ", " + pollAddressState;
+            var address = $("<div>").text("Address: " + pollAddress);
+            address.attr("class", "address-name");
 
-    newDiv.append(pollName);
-    newDiv.append(address);
-    newDiv.append(directions);
-    $("body").append(newDiv);
-  }
+            var hours = $("<div>").text("Hours: " + response.earlyVoteSites[i].pollingHours)
+
+            var directions = $("<button>").text("Find Directions");
+            directions.attr("class", "button-name");
+            
+            //append all individual info divs to one main div
+            newDiv.append(pollName);
+            newDiv.append(address);
+            newDiv.append(hours);
+            newDiv.append(directions);
+
+            // apend div with all info to html
+            $(".container-pollingplace").append(newDiv);
+        }
+    });
+});
