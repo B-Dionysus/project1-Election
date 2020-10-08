308c8332ff139fd5d99e089063bf6714a048f7b7
Benjamin Dionysus <benjamin@sixbynine.com>
2020-10-08 12:08:38 -0500

    Still working

--- scripts/openFEC.js
+++ scripts/openFEC.js
@@ -57,10 +57,13 @@ function titleCase(str){
     if(!str) return false;
     allStr=str.toLowerCase().split(" ");
     final="";
-    var pos=0;
     for(w of allStr){
+        var pos=0;
         if(w.charAt(0)==="("){
-            pos=1;
+            if(w.charAt(1)==="\""){
+                pos=2;
+            }   
+            else pos=1;
         }
         // Set up a variable that is the first character of the word and make it upperCase
         var l=w.charAt(pos).toUpperCase();
@@ -68,7 +71,8 @@ function titleCase(str){
         w=w.slice(pos+1);
         // Add in our new upperCase character
          w=l+w;
-         if(pos)w="("+w;
+         if(pos===1)w="("+w;
+         if(pos===2)w="(\""+w;
         final+=w+" ";
     }
     return final;
