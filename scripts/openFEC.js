fdfdc8592a2323886f8ea6a4306bb4f70a9fbc94
Benjamin Dionysus <benjamin@sixbynine.com>
2020-10-08 12:36:24 -0500

    Fixed formatting for nicknames

--- scripts/openFEC.js
+++ scripts/openFEC.js
@@ -1,5 +1,5 @@
 $(document).ready(init);
-
+var _rep;
 
 
 function init(){
@@ -13,14 +13,26 @@ function init(){
 // Displays name and party data for everyone who filed a presidential run in 2020
 // and puts it in #pres-data
 function displayPresident(response){
+    _rep=response;
     var pres=$("<p>").attr("id","president");
-    for(candidate of response.results){        
+    for(candidate of response.results){    
+        var filing=candidate.last_file_date;   
+        // Names come by default in all uppercase
+        // titleCase() is in ./scripts/utilities.js
         var name=titleCase(candidate.name);
+        // Names come by default last-name-first
+       // temp=name.split(",");
+        //name=temp[1]+temp[0];
+        // Unfortunately, the data may include honorifics at the end, without demarcation
+        // e.g. "Biden, Joseph R Jr," and these appear to be arbitrary
+        // e.g. "Jones, Take Mr.", so I don't think there's a clear way to sort them.
+        // Also, while I'm here, only of the presidential candidates is listed as
+        // "Title, Sheila Samm Mpresident". This is how they appear in the data set from the FEC. 
         if(candidate.party_full) var party=titleCase(candidate.party_full);
         else party="Unknown Party";
         status=candidate.incumbent_challenge_full.toLowerCase();
         // Let's limit this to only folks with an FEC filing from this year
-        if(filing.split("-")[0]==="2020" && filing.split("-")[1]>2)
+       if(filing.split("-")[0]==="2020" && filing.split("-")[1]>2)
          pres.append($("<p>").text(name+" is a member of the "+party).addClass(status));
     }
     $("#pres-data").append(pres);
@@ -53,32 +65,6 @@ function displayHouseAndSenate(response){
 
 
 
-// titleCase() 
-// takes "STRING HERE" or "string here" and returns "String Here"
-function titleCase(str){
-    if(!str) return false;
-    allStr=str.toLowerCase().split(" ");
-    final="";
-    for(w of allStr){
-        var pos=0;
-        if(w.charAt(0)==="("){
-            if(w.charAt(1)==="\""){
-                pos=2;
-            }   
-            else pos=1;
-        }
-        // Set up a variable that is the first character of the word and make it upperCase
-        var l=w.charAt(pos).toUpperCase();
-        // Remove the first character
-        w=w.slice(pos+1);
-        // Add in our new upperCase character
-         w=l+w;
-         if(pos===1)w="("+w;
-         if(pos===2)w="(\""+w;
-        final+=w+" ";
-    }
-    return final;
-}
 
 
 
