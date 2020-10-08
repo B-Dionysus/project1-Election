021f74e5c2fc0bc6aee735179ec572ae50963f0e
sarahlcox <sarahl.cox.22@gmail.com>
2020-10-08 14:15:20 -0500

    added countdown

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,24 @@
-// helloworld      var queryUrl = "https://www.googleapis.com/civicinfo/v2/elections?key=AIzaSyDDcoCWMnPNLsXimjEmRL85TOfhM9yPsA8"
-src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js";        
+// Set the date we're counting down to
+var countDownDate = new Date("Nov 3, 2020").getTime();
 
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+  // Display the result in the element with id="countdown"
+  document.getElementById("countdown").innerHTML = days + " DAYS";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
