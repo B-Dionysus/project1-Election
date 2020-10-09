f699d2c7e990c029f033b08370590357f6dc356c
Aaron Ray <aaray619@gmail.com>
2020-10-09 13:28:24 -0500

    fixed typos and added pseudo code

--- scripts/pollinglocations.js
+++ scripts/pollinglocations.js
@@ -1,18 +1,24 @@
 $(document).ready(function(){
-  
+    //get stored address
     localStorage.getItem("userAddress");
+    //push address info on the page
     pushInfo(localStorage.getItem("userAddress")); 
 
-    $("#userpolling").empty();
-    $("#earlypolling").empty();
+    
     function pushInfo(userAddress){
+        //clear existing infomration on page
+        $("#userpolling").empty();
+        $("#earlypolling").empty();
+        //call to API
         $.ajax({
             url: "https://civicinfo.googleapis.com/civicinfo/v2/voterinfo?electionId=7000&address=" + userAddress + "&key=AIzaSyDDcoCWMnPNLsXimjEmRL85TOfhM9yPsA8",
             method: "GET",
         }).then(function(response) {
+            //logic to check if API returns targeted info
+            //if yes then print infor on page
             if(response.hasOwnProperty("pollingLocations")=== true){
 
-                // $("#userpolling").empty();
+                //create div to hold info
                 var newDiv = $("<div>");
                 newDiv.attr("class", "callout small");
                 //call info from APi and create new div for each info with own class.
@@ -23,20 +29,18 @@ $(document).ready(function(){
                 var pollAddressLine1 = response.pollingLocations[0].address.line1
                 var pollAddressState = response.pollingLocations[0].address.state
                 var pollAddress =  pollAddressLine1 + " " + pollAddressCity + ", " + pollAddressState;
-                var address = $("<p>").text("Address: ").attr("class", "poll-address");
+                var address = $($("<p>").text("Address: ").attr("class", "poll-address"));
                 console.log(pollAddressCity)
                 address.append($("<span>").text(pollAddress).attr("class", "span"));
                 address.attr("class", "address-name");
 
                 var hours = $("<p>").text("Hours: ").attr("class", "poll-hours"); 
                 hours.append("<span>").text(response.pollingLocations[0].pollingHours).attr("class", "span");
-                // var hours = $("<div>").text("Hours: " + response.earlyVoteSites[i].pollingHours)
 
                 var directions = $("<button>").text("Find Directions");
                 directions.attr("class", "button-name");
                 // create an attr to store the polling address in the button
                 directions.attr("data-address", pollAddress);
-                
                 
                 //append all individual info divs to one main div
                 newDiv.append(pollName);
@@ -75,7 +79,6 @@ $(document).ready(function(){
 
                     var hours = $("<p>").text("Hours: ").attr("class", "poll-hours");
                     hours.append($("<span>").text("M-F: 8:30am-7pm || Sat-Sun: 9am-5pm").attr("class", "span"));
-                    // var hours = $("<div>").text("Hours: " + response.earlyVoteSites[i].pollingHours)
 
                     var directions = $("<button>").text("Find Directions");
                     directions.attr("class", "button-name");
@@ -100,6 +103,7 @@ $(document).ready(function(){
                     window.open("https://www.google.com/maps/dir/" + userAddress + "/" + $(this).attr("data-address"), '_blank');
                 })
             }
+            //if API response does not have targeted info then let user know
             else{
                 var newDiv = $("<div>").text("No polling information found for this location.").attr("class", "noInfo")
                 var otherDiv = $("<div>").text("No polling information found for this location.").attr("class", "noInfo")
@@ -108,13 +112,15 @@ $(document).ready(function(){
             }
         }); 
     }
-
+    //create a click event for submitting address on pollingpage
     $("#submitAddress").click(function(e){
         e.preventDefault();
+        //get text from input field
         var userAddress = $("#findtext").val().trim();
         addresslogic(userAddress);
 
     });
+    //create a function to display address from local storage if no user input on page
     function addresslogic(userAddress){
         if (userAddress === null) {
             $("#userpolling").empty();
@@ -131,4 +137,3 @@ $(document).ready(function(){
     }
 })
 
-// test
